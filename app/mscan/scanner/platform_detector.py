2e4b77b3c9c975fc8930e41e62e905f54b5140b2
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-02-05 02:22:28 +0200

    feat(platform_detector): lru cache

--- app/mscan/scanner/platform_detector.py
+++ app/mscan/scanner/platform_detector.py
@@ -1,11 +1,12 @@
 import platform
 from enum import Enum
-
+from functools import lru_cache
 
 class Platform(Enum):
     LINUX = 'linux'
     MAC = 'darwin'
     WINDOWS = 'windows'
 
+    @lru_cache()
     def __bool__(self):
         return platform.system().lower() == self.value
